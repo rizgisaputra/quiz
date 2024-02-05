18ea77cd9ede2ffd38fbd3332972d37748c58a6b
rizgisaputra <rizgisaputra326@gmail.com>
2024-02-05 17:13:21 +0700

    commit_day1

---
+++ resources/views/personal.blade.php
@@ -0,0 +1,77 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+    <link rel="stylesheet" href="../../css/personal.css">
+    <title>Hello, world!</title>
+  </head>
+  <body>
+    <div class="container-fluid mt-3">
+        <div class="container d-flex flex-column gap-3 justify-content-center">
+          <header>
+            <nav class="fixed-top">
+                <div class="navbar navbar-expand-lg w-100">
+                  
+                    <div
+                      class="container d-flex flex-row justify-content-between w-100 "
+                      
+                    >
+                      <div class="d-flex flex-row gap-2 align-items-center">
+                        <span class="fw-bold text-primary fs-4">RIZGI SAPUTRA</span>
+                      </div>
+                      <div class="my-profil d-flex flex-row">
+                        <span class="fw-bold text-primary fs-4">My Profil</span>
+                      </div>
+                    </div>
+                </div>
+            </nav>
+          </header>
+            <div class="image d-flex justify-content-center mt-5">
+               <img src="../../assets/img/image.jpg" alt="" class="rounded-circle" 
+               style=" width:15rem;
+               height:15rem;">
+            </div>
+            <div class="button d-flex justify-content-center">
+                <button type="button" class="btn btn-primary rounded mt-3 text-light fs-5 fw-bold btn-top" 
+                style=" width:10rem;
+                height:3rem;">
+                Click Here</button>
+            </div>
+            <div class="title d-flex flex-column gap-1 justify-content-center mt-3">
+                <span class="fs-1 fw-bold text-dark d-flex justify-content-center" >Web Profil Pribadi</span>
+                <span class="fs-4 text-dark d-flex justify-content-center">Web Developer</span>
+            </div>
+            <div class="content mt-3 d-flex justify-content-center ">
+                <span class="text-dark fs-5 d-flex justify-content-center text-center ">
+                  Hi, my name is Rizgi Saputra, I come from Kebumen, Central Java.
+                  I was born in Kebumen on February 3 2006 and I am currently 17
+                  years old. Now I live in Pondok Aren, Bintaro, South Tangerang. I
+                  am a fresh graduate and am interested in technology and want to
+                  have a career in technology. Now I am study on Staditek and i have
+                  get matery for example algoritma, then programing language like
+                  C++ . Beside that i get matery database especially postgresql and
+                  now i studying html, css and bootstrap.
+                </span>
+            </div>
+            <div class="contact d-flex flex-row gap-3 justify-content-center mb-5 mt-5 ">
+              <button type="button" class="btn btn-outline-primary rounded mt-3 text-dark fs-5 fw-bold d-flex justify-content-center " 
+              style=" width:10rem;
+              height:3rem;">
+                Message</button>
+                <button type="button" class="btn btn-primary rounded mt-3 text-light fs-5 fw-bold d-flex justify-content-center"
+                style=" width:10rem;
+                height:3rem;" >
+                  Contact</button>
+            </div>
+        </div>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
+  </body>
+</html>
